ba967546fef48efcfe0e95ef89c7f29bce518f18
DanielArnould <darn0005@student.monash.edu>
2023-12-18 07:21:37 +1100

    Started basic modal

--- src/components/HomeButtons.tsx
+++ src/components/HomeButtons.tsx
@@ -1,20 +1,82 @@
-import { Flex, Button } from "@chakra-ui/react";
+import {
+  Flex,
+  Button,
+  useDisclosure,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  VStack,
+  Input,
+} from "@chakra-ui/react";
 import {
   AiOutlineFileAdd,
   AiOutlineFolderOpen,
   AiOutlineLink,
 } from "react-icons/ai";
+import TaskItem from "./TaskItem";
+import { useState } from "react";
+import { tasksDb } from "../lib/dummy-data/tasksDb";
 
 const HomeButtons = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [searchQuery, setSearchQuery] = useState("");
+  const textboxes = Object.keys(tasksDb);
+
+  const filteredTextboxes = textboxes.filter((textbox) =>
+    textbox.toLowerCase().includes(searchQuery.toLowerCase()),
+  );
+
   return (
     <Flex flexDirection="column" gap={3}>
-      <Button
-        size="xl"
-        colorScheme="salmon"
-        leftIcon={<AiOutlineFileAdd fontSize={36} />}
-      >
-        Create a document from a PDF...
-      </Button>
+      <>
+        <Button
+          size="xl"
+          colorScheme="salmon"
+          leftIcon={<AiOutlineFileAdd fontSize={36} />}
+          onClick={onOpen}
+        >
+          Create a document from a PDF...
+        </Button>
+        <Modal
+          isOpen={isOpen}
+          onClose={onClose}
+          scrollBehavior="inside"
+          size="6xl"
+        >
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Modal Title</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <VStack>
+                <Input />
+                <Input type="file" />
+                <Input />
+                <Input
+                  placeholder="Search..."
+                  value={searchQuery}
+                  onChange={(e) => setSearchQuery(e.target.value)}
+                />
+                <VStack minH={"100%"}>
+                  {filteredTextboxes.map((textbox, index) => (
+                    <Input key={index} placeholder={textbox} />
+                  ))}
+                </VStack>
+              </VStack>
+            </ModalBody>
+
+            <ModalFooter>
+              <Button colorScheme="salmon" mr={3} onClick={onClose}>
+                Create
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </>
       <Button
         size="xl"
         variant="outline"
